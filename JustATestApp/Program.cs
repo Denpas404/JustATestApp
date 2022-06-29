6cd1f52eee33d001437555b1725839a4021852a7
DenPas404 / D.Paaske <denpasdk@gmail.com>
2022-06-29 14:04:49 +0200

    just a test app

---
+++ JustATestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustATestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "infobase.txt";
+            string path = Path.Combine(Environment.CurrentDirectory, @"Data\", fileName);
+
+            string txt = File.ReadAllText(path);
+
+            Console.WriteLine(txt);
+
+            Console.ReadKey();
+        }
+    }
+}
